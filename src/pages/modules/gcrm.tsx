57499cba06dd47605766d098eeb99bd75d4359e8
Sinan <sinan.taylan@outlook.com>
2024-03-15 10:52:30 +0300

    Set Nav Items

--- src/pages/modules/gcrm.tsx
+++ src/pages/modules/gcrm.tsx
@@ -4,14 +4,82 @@ import {
   Box, Button, Card, CardActions, CardContent, Grid, Typography
 } from "@mui/material"
 import { useNavigate } from "react-router-dom"
-import {useAuthStore} from "gstore/store";
+import {useAuthStore, useLayoutStore} from "gstore/store";
+// import { Business, List, Person } from '@mui/icons-material';
+import BusinessIcon from '@mui/icons-material/Business';
+import ListIcon from '@mui/icons-material/List';
 
 interface IGCRMProps { }
 
 const GCRM: React.FunctionComponent<IGCRMProps> = () => {
   const navigate = useNavigate();
   const { currentUser, authToken } = useAuthStore();
+  const { setNavItems } = useLayoutStore();
 
+  React.useEffect(() => {
+    setNavItems([
+      {
+        id: 'parentcustomer',
+        title: 'Şirket Yönetimi',
+        translate: 'PLATFORM_COMPANY',
+        type: 'collapse', // 'collapse', 'group', 'item'
+        icon: <BusinessIcon />,
+        children: [
+          {
+            id: 'parentcustomer.company',
+            title: 'Şirketler',
+            type: 'item',
+            icon: <ListIcon />,
+            url: '/gcloud/olcer/gcrm'
+          },
+          {
+            id: 'parentcustomer.license',
+            title: 'Şubeler',
+            type: 'item',
+            icon: <ListIcon />,
+            url: '/gcloud/olcer/gcrm'
+          },
+          {
+            id: 'parentcustomer.license',
+            title: 'Departmanlar',
+            type: 'item',
+            icon: <ListIcon />,
+            url: '/gcloud/olcer/gcrm'
+          },
+          {
+            id: 'parentcustomer.license',
+            title: 'Depolar',
+            type: 'item',
+            icon: <ListIcon />,
+            url: '/gcloud/olcer/gcrm'
+          },
+        ]
+      },
+      {
+        id: 'parentcustomer',
+        title: 'Kullanıcı Yönetimi',
+        translate: 'PLATFORM_COMPANY',
+        type: 'collapse', // 'collapse', 'group', 'item'
+        icon: <BusinessIcon />,
+        children: [
+          {
+            id: 'parentcustomer.company',
+            title: 'Kullanıcılar',
+            type: 'item',
+            icon: <ListIcon />,
+            url: '/gcloud/olcer/gcrm'
+          },
+          {
+            id: 'parentcustomer.license',
+            title: 'Kullanıcı Rolleri',
+            type: 'item',
+            icon: <ListIcon />,
+            url: '/gcloud/olcer/gcrm'
+          },
+        ]
+      },
+    ])
+  }, [])
 
   return (
     <Box>
